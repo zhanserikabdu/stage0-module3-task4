d16c4ddd258860fcd1141739472a5304d1f4f1b2
NANO <File changed>
2022-07-03 17:39:20 +0600

    First submission

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a = 'c';
+        System.out.println(a);
+        char b = 'h';
+        System.out.println(b);
+        char c = 'a';
+        System.out.println(c);
+        char d = 'r';
+        System.out.println(d);
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-//         first = 10_000_000_000L;
-//         second = 'a';
-//         third = 5 > 9;
-//         forth = 12.90434323324342;
-//         fifth = 12.3F;
-//         sixth = 1;
-//         seventh = 129;
+      long  first = 10_000_000_000L;
+       char  second = 'a';
+         boolean   third = 5 > 9;
+       double forth = 12.90434323324342;
+     float   fifth = 12.3F;
+      int sixth = 1;
+      short  seventh = 129;
     }
 }
