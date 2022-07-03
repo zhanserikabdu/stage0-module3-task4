ecff0f0b6e550a9c62aba58ecce9923df8371c65
NANO <File changed>
2022-07-03 19:17:07 +0600

    5 submission

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-      long  first = 10_000_000_000L;
+       long  first = 10_000_000_000L;
        char  second = 'a';
-       boolean third = false;
+       boolean third = 5 > 9;
        double forth = 12.90434323324342;
-     float   fifth = 12.3F;
-      int sixth = 1;
-      short  seventh = 129;
+       float   fifth = 12.3F;
+       int sixth = 1;
+       short  seventh = 129;
     }
 }
